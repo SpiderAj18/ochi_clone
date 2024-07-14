2205fbbd1953268cfb8d8f8d1b44724344a20362
Ajay Gupta <officialajay1810@gmail.com>
2024-07-14 15:40:28 +0530

    navbar created

---
+++ src/components/Landingpage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Landingpage() {
+  return (
+    <div className='w-full h-screen bg-zinc-950'>
+        <div className='textstructure'>
+            <div className="masker">
+                <h1 className="uppercase text-9xl font-['Founders_Grotesk_Text']">we create</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Landingpage
